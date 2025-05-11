e8b07cdeca7d4325c73d7ffcf8b323f38f065590
Gonmex <kjmvlkxlkjkjlkxvjx>
2025-05-11 14:25:32 +0700

    Создание счёта и перезапуска

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ ball_sc = image.load("ball.png")
 platform1_sc = image.load("platform 1.png")
 platform2_sc = image.load("platform 2.png")
 font1 = font.Font(None,100)
+font2 = font.Font(None,70)
+
+count = 0
+count_text = font2.render(f"Счёт: {count}",True,(29, 63, 87))
 
 class Sprite():
     def __init__(self,image_p,x,y):
@@ -32,7 +36,7 @@ class Ball(Sprite):
         self.speed_y = 5
 
     def move(self):
-        global lose,lose_text
+        global lose,lose_text,restart_text,count,count_text
         if self.rect.y <= 0:
             self.speed_y *= -1
         elif self.rect.y >= 870:
@@ -40,14 +44,21 @@ class Ball(Sprite):
         
         if sprite.collide_rect(self,platform1):
             self.speed_x *= -1
+            count += 1
+            count_text = font2.render(f"Счёт: {count}",True,(29, 63, 87))
         elif sprite.collide_rect(self,platform2):
             self.speed_x *= -1
+            count += 1
+            count_text = font2.render(f"Счёт: {count}",True,(29, 63, 87))
         if self.rect.x <= 0:
             lose = 1
             lose_text = font1.render(f"Игрок {lose} проиграл",True,(0,0,0))
+            restart_text = font1.render("Нажмите 'R' для перезапуска",True,(0,0,0))
         if  self.rect.x >= 1370:
             lose = 2
             lose_text = font1.render(f"Игрок {lose} проиграл",True,(255,0,0))
+            restart_text = font1.render("Нажмите 'R' для перезапуска",True,(0,0,0))
+
 
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
@@ -66,6 +77,16 @@ while mod == 0:
     for e in event.get():
         if e.type == QUIT:
             mod = 1
+        if lose == 1 or lose == 2:
+            if e.type == KEYDOWN:
+                if e.key == K_r:
+                    ball = Ball(ball_sc,600,600)
+                    platform1 = Platform(platform1_sc,100,500)
+                    platform2 = Platform(platform2_sc,1200,500)
+                    count = 0
+                    count_text = font2.render(f"Счёт: {count}",True,(29, 63, 87))
+                    lose = 0
+
     win.fill((100,100,100))
 
 
@@ -77,8 +98,11 @@ while mod == 0:
     platform1.draw()
     platform2.draw()
 
+
+    win.blit(count_text,(1250,40))
     if lose == 1 or lose == 2:
-        win.blit(lose_text,(300,500))
+        win.blit(lose_text,(430,450))
+        win.blit(restart_text,(270,900))
 
 
     tick.tick(60)
